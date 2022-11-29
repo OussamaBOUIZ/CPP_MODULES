535d0e09c58bea04db245086b0c3247ea1f34ad0
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-11-29 18:39:23 +0100

    Version 2 of the megaphone program C++ manner

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -2,6 +2,8 @@
 #include <cctype>
 #include <string>
 
+// USING THE C MANNER WE'LL HAVE THIS
+/*
 void	strToUpper(char *str)
 {
 	for (int i = 0; str[i]; i++)
@@ -23,5 +25,45 @@ int main(int ac, char **av)
 	std::cout << std::endl;
 	return (0);
 }
+*/
 
-// LET'S NOW REMAKE THIS PROBLEM IN A C++ MANNER
+
+// LET'S NOW RESOLVE THIS EXERCISE IN A C++ MANNER
+
+class Megaphone {
+	private:
+		char			**sentences;
+		unsigned int	numOfSentences;
+		void	strToUpper(char *str)
+		{
+			for (int i = 0; str[i]; i++)
+				str[i] = toupper(str[i]);
+		}
+	public:
+		Megaphone(char **sentences, unsigned int numOfSentences){
+			this->sentences = sentences;
+			this->numOfSentences = numOfSentences;
+		}
+		void	speakLoudly(void)
+		{
+			if (!this->numOfSentences)
+			{
+				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+				return ;
+			}
+			for (unsigned int i = 0; i < this->numOfSentences; i++)
+			{
+				strToUpper(this->sentences[i]);
+				std::cout << this->sentences[i];
+			}
+			std::cout << std::endl;
+		}
+};
+
+
+int	main(int ac, char **av)
+{
+	Megaphone newMegaphone = Megaphone(av + 1, ac - 1);
+	newMegaphone.speakLoudly();
+	return 0;
+}
