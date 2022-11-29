556ccace8b6fd84caf708ef92bac5d0af54f4844
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-11-29 15:56:00 +0100

    Version 1 of the megaphone program C manner

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <cctype>
+#include <string>
 
+void	strToUpper(char *str)
+{
+	for (int i = 0; str[i]; i++)
+		str[i] = toupper(str[i]);
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		strToUpper(av[i]);
+		std::cout << av[i] << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
+
+// LET'S NOW REMAKE THIS PROBLEM IN A C++ MANNER
