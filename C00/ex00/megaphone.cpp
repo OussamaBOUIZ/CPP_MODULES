d74d2491116e138727972ece30c07f07b8a25149
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-12-02 18:24:58 +0100

    Refactor: Defining member functions outside of the class

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -2,64 +2,42 @@
 #include <cctype>
 #include <string>
 
-// USING THE C MANNER WE'LL HAVE THIS
-/*
-void	strToUpper(char *str)
+class Megaphone {
+	private:
+		char			**sentences;
+		unsigned int	numOfSentences;
+		void	strToUpper(char *str);
+	public:
+		Megaphone(char **sentences, unsigned int numOfSentences);
+		void	speakLoudly(void);
+};
+
+Megaphone::Megaphone(char **sentences, unsigned int numOfSentences)
+{
+	this->sentences = sentences;
+	this->numOfSentences = numOfSentences;
+}
+
+void	Megaphone::strToUpper(char *str)
 {
 	for (int i = 0; str[i]; i++)
 		str[i] = toupper(str[i]);
 }
 
-int main(int ac, char **av)
+void	Megaphone::speakLoudly(void)
 {
-	if (ac == 1)
+	if (!this->numOfSentences)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		return ;
 	}
-	for (int i = 1; i < ac; i++)
+	for (unsigned int i = 0; i < this->numOfSentences; i++)
 	{
-		strToUpper(av[i]);
-		std::cout << av[i] << " ";
+		strToUpper(this->sentences[i]);
+		std::cout << this->sentences[i];
 	}
 	std::cout << std::endl;
-	return (0);
 }
-*/
-
-
-// LET'S NOW RESOLVE THIS EXERCISE IN A C++ MANNER
-
-class Megaphone {
-	private:
-		char			**sentences;
-		unsigned int	numOfSentences;
-		void	strToUpper(char *str)
-		{
-			for (int i = 0; str[i]; i++)
-				str[i] = toupper(str[i]);
-		}
-	public:
-		Megaphone(char **sentences, unsigned int numOfSentences){
-			this->sentences = sentences;
-			this->numOfSentences = numOfSentences;
-		}
-		void	speakLoudly(void)
-		{
-			if (!this->numOfSentences)
-			{
-				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-				return ;
-			}
-			for (unsigned int i = 0; i < this->numOfSentences; i++)
-			{
-				strToUpper(this->sentences[i]);
-				std::cout << this->sentences[i];
-			}
-			std::cout << std::endl;
-		}
-};
-
 
 int	main(int ac, char **av)
 {
