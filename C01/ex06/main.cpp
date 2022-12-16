b2f74795e1ae401f4a382c9f6add1cae5fc36c67
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-12-16 17:52:03 +0100

    Adding an Error message in case of incomplete arguments

--- C01/ex06/main.cpp
+++ C01/ex06/main.cpp
@@ -5,7 +5,7 @@ int main ( int ac, char **av )
 	Harl	Harly;
 
 	if (ac != 2)
-		return (1);
+		{std::cerr << "NO LEVEL WAS SPECIFIED!!" << std::endl; return (1);}
 	Harly.complain(av[1]);
 	return (0);
 }
