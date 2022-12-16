b0b4b1b9f60aa6dba9b1e186add896f07e5b22c7
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-12-16 17:19:39 +0100

    Fix: checking for the existing of a weapon attacking for HumanB class

--- C01/ex03/HumanB.cpp
+++ C01/ex03/HumanB.cpp
@@ -1,11 +1,14 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 
-HumanB::HumanB(std::string name): name(name) , weapon(NULL){};
+HumanB::HumanB(std::string name): weapon(NULL), name(name){};
 
 void	HumanB::attack( void )
 {
-	std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+	if (weapon)
+		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+	else
+		std::cout << name << "has no weapon" << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &newWeapon)
