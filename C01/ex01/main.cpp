db8c8f5a27328404bd344c6f946428f3d5a3bf8e
Oussama Bouizgarne <obouizga@student.1337.ma>
2022-12-16 15:17:10 +0100

    Update: Makefile and Protecting the new operator

--- C01/ex01/main.cpp
+++ C01/ex01/main.cpp
@@ -5,10 +5,8 @@ int	main(int ac, char **av)
 	Zombie	*z;
 
 	if (ac < 3)
-		return (1);
+		{std::cerr << "Incomplete Arguments" << std::endl; return (1);}
 	z = zombieHorde(atoi(av[1]), av[2]);
-	if (z == nullptr)
-		return (1);
 	for (int i = 0; i < atoi(av[1]); i++)
 		z[i].announce();
 	delete [] z;
--- C01/ex01/zombieHorde.cpp
+++ C01/ex01/zombieHorde.cpp
@@ -6,7 +6,9 @@ Zombie	*zombieHorde( int N, std::string name )
 
 	if (N <= 0)
 		return (nullptr);
-	zHorde = new Zombie[N];
+	zHorde = new (std::nothrow) Zombie[N];
+	if (zHorde == nullptr)
+		{std::cerr << "new failed" << std::endl; exit(0);}
 	for (int i = 0; i < N; i++)
 		zHorde[i].name = name;
 	return (zHorde);
