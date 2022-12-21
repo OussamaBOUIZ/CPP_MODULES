1e7971c0cecfd1861b1111180a800c4fde7c5c8f
Oussama BOUIZ <65982891+OussamaBOUIZ@users.noreply.github.com>
2022-12-21 16:50:21 +0000

    Defining Fixed member functions

--- C02/ex00/Fixed.cpp
+++ C02/ex00/Fixed.cpp
@@ -1,11 +1,34 @@
 #include "Fixed.hpp"
+#include <iostream>
 
 Fixed::Fixed ( void )
 {
     fixedPointNumber = 0;
+    std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::~Fixed ( void )
 {
-    std::cout << "Destructor called " std::endl;
+    std::cout << "Destructor called " << std::endl;
 }
+
+/* LET'S DEFINE THE COPY CONSTRUCTOR*/
+Fixed::Fixed ( const Fixed &obj)
+{
+    fixedPointNumber = obj.fixedPointNumber;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed   &Fixed::operator= (const Fixed &obj)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    fixedPointNumber = obj.fixedPointNumber;
+    return (*this);
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (fixedPointNumber);
+}
+
