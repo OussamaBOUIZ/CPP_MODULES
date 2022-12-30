ca7e183d8ad4888045e92de44eb55a9c07c2ccb1
Oussama BOUIZ <65982891+OussamaBOUIZ@users.noreply.github.com>
2022-12-30 15:12:58 +0000

    Docs: INHERITANCE

--- sdbox/inherit.cpp
+++ sdbox/inherit.cpp
@@ -30,16 +30,39 @@ void    Camel::grunt( void )
     std::cout << "Grunting ... "<< std::endl;
 }
 
+class A {
+    public:
+    A(int a, int b) : a(a), b(b) {};
+    A &operator = (const A &obj)
+    {
+        std::cout << "== called" << std::endl;
+        a = obj.a + 5;
+        b = obj.b + 10;
+        return (*this);
+    }
+    void    displayMembers( void ) const
+    {
+        std::cout << "a : " << a  << std::endl;
+        std::cout << "b : " << b << std::endl;
+    }
+    protected:
+        int a, b;
+};
+
+class B: public A {
+    public:
+        B(int a, int b) : A(a, b) {};
+        B   &operator= (const B &obj) : opera 
+};
+
 int main ( void )
 {
-    Camel   camel;
+    B myclass(4, 7);
 
-    camel.drive();
-    camel.eat();
-    camel.grunt();
-    camel.sleep();
-    Dog dog1;
+    B   anotherClass = myclass;
+    myclass.displayMembers();
+    anotherClass.displayMembers();
 
-    dog1.eat();
     return (0);
 }
+
