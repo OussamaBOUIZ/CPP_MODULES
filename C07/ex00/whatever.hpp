bfd2783910f0114a8677819da880a3c62fe35642
Oussama Bouizgarne <obouizga@student.1337.ma>
2023-01-10 10:14:35 +0100

    C07/ex00: done

---
+++ C07/ex00/whatever.hpp
@@ -0,0 +1,23 @@
+template <typename T> 
+// template <typename T1> 
+
+void	swap( T &a, T &b )
+{
+	T temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T	min( T a, T b )
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T	max( T a, T b )
+{
+	return (a > b ? a : b);
+}
