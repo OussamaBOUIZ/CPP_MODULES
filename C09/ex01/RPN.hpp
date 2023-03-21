18b0f7a2ab379b7d28ba188b987402dfcd63f5ce
Oussama Bouizgarne <obouizga@student.1337.ma>
2023-03-21 12:14:13 +0000

    isOperator

--- C09/ex01/RPN.hpp
+++ C09/ex01/RPN.hpp
@@ -1,5 +1,7 @@
 #ifndef __RPN_H__
 # define __RPN_H__
+# include <stack>
+typedef std::string string;
 
 class RPN {
 	public:
@@ -7,7 +9,11 @@ class RPN {
 		~RPN ( void );
 		RPN ( const RPN & );
 		RPN	&operator= ( const RPN & );
-};
+	private:
+		bool	_isOperator ( string & );
+		std::stack<string>	_abacus;
 
+		
+};
 
 #endif
